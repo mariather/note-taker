4ded1abbb54d838e4bb5625f5a2d238f07a88242
aleja <92136921+mariather@users.noreply.github.com>
2022-08-20 15:25:09 -0700

    fix routes files and add comments

--- server.js
+++ server.js
@@ -1,53 +1,62 @@
+//dependencies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const db = require('./Develop/db/db.json');
 
+// sets the server up
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
+// static middleware
 app.use(express.static('public'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+//API route and Get request
 app.get("/api/notes", (req, res) => {
   console.log(`Hitting the API/Notes Route`);
   res.json(db);
 })
 
+//Post request
 app.post("/api/notes", (req, res) => {
   console.log(`Hitting the API/Notes Route (with post request)`);
 
   let newNote = req.body;
   newNote.id = uuidv4();
   db.push(newNote);
-  fs.writeFileSync("./db/db.json", JSON.stringify(db), (err) => {
+  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(db), (err) => {
     if(err) throw err;
   });
   res.send(db)
 })
 
+// API route and delete request
 app.delete("/api/notes/:id", (req, res) => {
   db.forEach((note, i) => {
     if (note.id === req.params.id) {db.splice(i, 1)}
   })
 
  
-  fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
+  fs.writeFile("./Develop/db/db.json", JSON.stringify(db), (err) => {
     if(err) throw err;
   })
   res.send(db)
 })
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/index.html')));
 
+
+//listening
 app.listen(PORT, () =>
 {
   console.log(`Listening at ${PORT}`);
 })
 
 
-//note: fix port and do readme
+
